001c460bfab1e29bdb87cad655d1e07e6e29beeb
Bina-man <binasisayet8790@gmail.com>
2022-07-11 20:23:58 +0300

    Actor and director who cooperated at least three times

---
+++ Leet-code/SQL/EASY/Actors_and_Directors_Who_Cooperated At_Least_Three_Times.sql
@@ -0,0 +1,36 @@
+"""
+Actors and Directors Who Cooperated At Least Three Times - Easy
+
+Input: 
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output: 
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+"""
+
+"""
+Solution
+"""
+
+select actor_id, director_id from ActorDirector
+where director_id =
+(select director_id from ActorDirector
+group by director_id, actor_id 
+having count(director_id) >= 3)
+limit 1;
