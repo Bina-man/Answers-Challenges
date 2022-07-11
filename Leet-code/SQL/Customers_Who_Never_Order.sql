a257e0a81c85bfb8ab1bb93aca283ab33f0b79fa
Bina-man <binasisayet8790@gmail.com>
2022-07-11 15:36:18 +0300

    Customers who never order

---
+++ Leet-code/SQL/Customers_Who_Never_Order.sql
@@ -0,0 +1,36 @@
+"""
+Customers Who Never Order - Easy
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+"""
+
+select name as "Customers" 
+from Customers
+where Customers.id not in
+(select customerId from Orders)
