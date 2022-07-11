f21dd6bea1077fbaf93f2d6b47e81149eb55956c
Bina-man <binasisayet8790@gmail.com>
2022-07-11 16:07:05 +0300

    Departement highest salary

---
+++ Leet-code/SQL/MEDIUM/Department_Highest_Salary.sql
@@ -0,0 +1,47 @@
+"""
+Departement of the highest salary
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
+"""
+
+"""
+Solution:
+"""
+
+select d.name as "Department", 
+    e1.name as "Employee",
+    e1.salary as "Salary"
+from Employee as e1 inner join Departement as d 
+    on e1.departmentId = d.id
+where e1.salary = 
+    (select Max(salary)
+        from Employee as e2
+        where e2.departmentId = e1.departmentId
+    )
