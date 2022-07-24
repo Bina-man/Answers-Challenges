b62027ce0c129a572f1879d40302e9ba1222f56a
Bina-man <binasisayet8790@gmail.com>
2022-07-24 03:33:09 +0300

    Cutomer1.sql

---
+++ Hacker-Rank/SQL/EASY/Customer1.sql
@@ -0,0 +1,18 @@
+"""
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+"""
+"""
+SOLUTION
+"""
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE REGEXP_LIKE(CITY, '^[AEIOU]') AND REGEXP_LIKE(CITY, '[AEIOU]$')
